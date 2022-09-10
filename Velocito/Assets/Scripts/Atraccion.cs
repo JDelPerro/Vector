c24fa521adddd73cf7bdb87ad1f4e62287da8306
JDelPerro <andres.duquel@upb.edu.co>
2022-09-10 03:21:50 -0500

    Orbita

---
+++ Velocito/Assets/Scripts/Atraccion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+
+public class Atraccion : MonoBehaviour
+{
+    private MyRector position;
+    private MyRector acceleration;
+    [SerializeField] float mass = 1;
+    [SerializeField] float targetmass = 1;
+    [SerializeField] private MyRector velocity;
+    [SerializeField] Transform Target;
+    [SerializeField] [Range(0, 1)] float gravity = -9.8f;
+
+    private void Start()
+    {
+        position = transform.position;
+    }
+    public void Move()
+    {
+        velocity += acceleration * Time.fixedDeltaTime;
+        position += velocity * Time.fixedDeltaTime;
+        transform.position = position;
+
+    }
+    private void FixedUpdate()
+    {
+        MyRector r = Target.position - transform.position;
+        float weightScalar = mass * gravity;
+        MyRector weight = new MyRector(0, weightScalar);
+        acceleration *= 0f;
+        float Magnituder = r.magnitude;
+        MyRector F = r.normalized * (1 / targetmass * mass / Magnituder * Magnituder);
+
+        ApplyForce(F);
+        Move();
+        F.Draw2(position, Color.red);
+    }
+    private void Update()
+    {
+        velocity.Draw2(position, Color.green);
+    }
+
+    void ApplyForce(MyRector force)
+    {
+        acceleration += force * (1f / mass);
+    }
+}
---
+++ Velocito/Assets/Scripts/MyRector.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public struct MyRector
+{
+    public float magnitude => Mathf.Sqrt(x * x + y * y);
+    public float x;
+    public float y;
+
+    public MyRector normalized
+    {
+        get
+        {
+            float distance = magnitude;
+            if (distance < 0.0001f)
+            {
+                return new MyRector(0, 0);
+            }
+            return new MyRector(x / distance, y / distance);
+        }
+    }
+    public MyRector(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public override string ToString()
+    {
+        return $"[{x},{y}]";
+    }
+    public static implicit operator Vector3(MyRector a)
+    {
+        return new Vector3(a.x, a.y, 0);
+    }
+    public MyRector Lerp(MyRector myVector, float t)
+    {
+        return this + (myVector - this) * t;
+    }
+    public void Draw(Color color)
+    {
+        Debug.DrawLine(new Vector3(0, 0, 0), this, color, 0);
+
+    }
+    public void Draw2(MyRector origin, Color color)
+    {
+        Debug.DrawLine(origin, this + origin, color, 0);
+
+    }
+    public void Normalize()
+    {
+        float magnitudeCache = magnitude;
+        if (magnitudeCache < 0.001)
+        {
+            x = 0;
+            y = 0;
+        }
+        else
+        {
+            x = x / magnitudeCache;
+            y = y / magnitudeCache;
+        }
+    }
+    public static MyRector operator +(MyRector a, MyRector b)
+    {
+        return new MyRector(a.x + b.x, a.y + b.y);
+
+    }
+    public static MyRector operator -(MyRector a, MyRector b)
+    {
+        return new MyRector(a.x - b.x, a.y - b.y);
+
+    }
+    public static MyRector operator *(MyRector a, float n)
+    {
+        return new MyRector(a.x * n, a.y * n);
+
+    }
+    public static implicit operator MyRector(Vector3 a)
+    {
+        return new MyRector(a.x, a.y);
+    }
+}
